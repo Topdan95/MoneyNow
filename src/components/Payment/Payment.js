588e81c7a029c8d1b0f5e80bd3fe0f557e81679f
Topdan95 <topdan95@gmail.com>
2023-01-01 14:08:08 +0100

    worked on the initial commit

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "./Payment.css";
 import bank from "../../Images/bank.svg";
 import card from "../../Images/card.svg";
@@ -6,40 +6,49 @@ import card from "../../Images/card.svg";
 const Payment = () => {
   return (
     <div className="payment back">
-        <div className="head">
-            <h2>HELLO VICTOR!</h2>
-            <p>Add Your Preferred Payment Method</p>
-        </div>
+      <div className="head">
+        <h2 className="header">HELLO VICTOR!</h2>
+        <p>Add Your Preferred Payment Method</p>
+      </div>
       <div className="choice">
         <div className="methods pay">
-            <h5>Choose Your Primary Payment Method</h5>
-            <p>Please add your account’s primary payment method which will be used to<br /> 
-                pay for your contributions . Don’t worry, you can always change this later</p>
-        <h6>Select a Payment Method</h6>
-        <div className="box">
-        <img className="iconss" src={bank} alt="bank" />
-        <p>Bank account</p>
-        <h6>Pay via bank transfer</h6>
-        <div className="bttns">
-            <button className="add">Add Bank</button>
-        </div>
-        </div>
-        <div className="box1">
-        <img className="iconss1" src={card} alt="card" />
-        <p>Credit or Debit Card</p>
-        <h6>Pay via card</h6>
-        <div className="bttns1">
-            <button className="add1">Add Card</button>
-        </div>
-        </div>
-        <div className="bttns2">
-            <button className="ads">Go Back</button>
-            <button className="adds">Continue</button>
-        </div>
+          <h5>Choose Your Primary Payment Method</h5>
+          <p>
+            Please add your account’s primary payment method which will be used
+            to pay for your contributions . Don’t worry, you can always change
+            this later
+          </p>
+          <h6>Select a Payment Method</h6>
+          <div className="box">
+            <div className="contner">
+              <img className="iconss" src={bank} alt="bank" />
+              <div>
+                <p>Bank account</p>
+                <h6>Pay via bank transfer</h6>
+              </div>
+            </div>
+
+            <button className="add butn btn-pry">Add Bank</button>
+          </div>
+          <div className="box">
+            <div className="contner">
+              <img className="iconss" src={card} alt="card" />
+              <div>
+                <p>Credit or Debit Card</p>
+                <h6>Pay via card</h6>
+              </div>
+            </div>
+
+            <button className="add1 butn btn-pry">Add Card</button>
+          </div>
+          <div className="bttns2">
+            <button className="ads butn btn-white">Go Back</button>
+            <button className="adds butn btn-pry">Continue</button>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Payment
+export default Payment;
--- src/components/Referral/Referral.js
+++ src/components/Referral/Referral.js
@@ -1,39 +1,63 @@
-import React from 'react';
+import React from "react";
 import "./Referral.css";
 import invitation from "../../Images/invitation.svg";
 import signup from "../../Images/signup.svg";
 import gift from "../../Images/gift.svg";
+import email from "../../Images/email.svg";
+import light from "../../Images/light.svg";
 
 const Referral = () => {
   return (
     <div className="referral back">
-        <div className="refer">
-            <h2>Referrals</h2>
-            <p>Refer your friends to  Money<span>Now</span>, if they sign up, you  enjoy N200.00 on every referrals</p>
+      <div className="refer">
+        <h2 className="header">Referrals</h2>
+        <p>
+          Refer your friends to Money<span className="text-blue">Now</span>, if
+          they sign up, you enjoy N200.00 on every referrals
+        </p>
         <div className="referrals">
-        <div className="invite friends">
+          <div className="friends">
             <img className="iv" src={invitation} alt="invitation" />
-            <h6>Invite your friends<br/>with the referral code</h6>
-        </div>
-        <div className="they friends">
+            <h6>Invite your friends with the referral code</h6>
+          </div>
+          <div className="friends">
             <img className="sign" src={signup} alt="signup" />
             <h6>They sign up</h6>
-        </div>
-        <div className="get friends">
+          </div>
+          <div className="friends">
             <img className="gift" src={gift} alt="gift" />
-            <h6>You get N200.00<br/>on every referral</h6>
-        </div>
+            <h6>You get N200.00 on every referral</h6>
+          </div>
         </div>
         <div className="texxt">
-        <h5>Invite your friends</h5>
-        <h6>Insert your friends’ addresses and send them invitations to join Money<span>Now</span></h6>
-        <input type="text" Email address/>
-
+          <h5>Invite your friends</h5>
+          <h6>
+            Insert your friends’ addresses and send them invitations to join
+            Money<span className="text-blue">Now</span>
+          </h6>
+          <div className="message">
+            <input type="text" name="email" placeholder="Email Address" />
+            <img className="myy" src={email} alt="email" />
+          </div>
         </div>
+        <div className="texxt">
+          <h5>Share the referral code</h5>
+          <h6>
+            You can also share your referral code by copying and sending or
+            sharing it on your social media Money
+            <span className="text-blue">Now</span>
+          </h6>
+          <div className="test">
+          <input type="text" name="email" placeholder="AB0AJr4G" />
+          <img className="myyy" src={light} alt="light" />
+          </div>
+          <div className="performance">
+            <button className="view bttns3">View Referral Performance</button>
+          </div>
         </div>
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Referral
+export default Referral;
